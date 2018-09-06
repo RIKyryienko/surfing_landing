fd8d006984f8c039ec0d6be297826fa3ea9099bd
ruslan_k <r.i.kyryienko@gmail.com>
2018-09-06 18:58:35 +0300

    Change setting project

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,33 @@
-const gulp = require('gulp'),
-    sass = require('gulp-sass');
+const   gulp        = require('gulp'),
+        // sass        = require('gulp-sass'),
+        pug         = require('gulp-pug'),
+        browserSync = require('browser-sync');
 
 
-gulp.task('sass', function() {
-    const header = gulp.src("app/blocks/header/header-sass/*.sass")
-        .pipe(sass())
-        .pipe(gulp.dest("app/blocks/header/header-css"));
+// gulp.task('sass', function() {
+//     const header = gulp.src("app/blocks/header/header-sass/*.sass")
+//         .pipe(sass())
+//         .pipe(gulp.dest("app/blocks/header/header-css"));
+//     return sass(header)
+// });
 
-
-    return sass(header)
+gulp.task('pug', function () {
+    gulp.src("app/index-pug/*.pug")
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest("app/"));
 });
+
+gulp.task('browserSync', function () {
+   browserSync({
+       server: 'app/'
+   }),
+       notification: false;
+});
+
+gulp.task('watch', ['sass', 'pug', 'browserSync'], function () {
+    gulp.watch("app/blocks/header/header-sass/*.sass");
+    gulp.watch("app/index-pug/*.pug");
+});
+
